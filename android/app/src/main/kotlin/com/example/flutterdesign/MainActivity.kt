36290e8be54a647f94722ec2d74ccd326fbc22e2
Ali-tech-tech <alijhla01@gmail.com>
2024-07-31 10:04:44 +0300

    flutter design  day 1

---
+++ android/app/src/main/kotlin/com/example/flutterdesign/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutterdesign
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/day_1.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+
+class Day1 extends StatelessWidget {
+  const Day1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(244, 243, 243, 1),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.black87,
+            )),
+      ),
+      body: SafeArea(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: double.infinity,
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius:
+                    BorderRadius.vertical(bottom: Radius.circular(30))),
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Find Your",
+                  style: TextStyle(color: Colors.black87, fontSize: 25),
+                ),
+                const SizedBox(
+                  height: 3,
+                ),
+                const Text(
+                  "Inspiration",
+                  style: TextStyle(color: Colors.black, fontSize: 40),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      color: const Color.fromRGBO(244, 243, 243, 1),
+                      borderRadius: BorderRadius.circular(15)),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefix: Icon(
+                          Icons.search,
+                          color: Colors.black87,
+                        ),
+                        hintText: "Search you'r looking for",
+                        hintStyle: TextStyle(color: Colors.grey, fontSize: 15)),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Promo Today",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  height: 200,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      promoCard("assets/images/1.jpg"),
+                      promoCard("assets/images/2.jpg"),
+                      promoCard("assets/images/3.jpg"),
+                      promoCard("assets/images/4.jpg"),
+                      promoCard("assets/images/5.jpg"),
+                      promoCard("assets/images/6.jpg"),
+                      promoCard("assets/images/7.jpg"),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  height: 150,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    image: const DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage("assets/images/8.jpg")),
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        gradient: LinearGradient(
+                            begin: Alignment.bottomRight,
+                            stops: const [
+                              0.3,
+                              0.9
+                            ],
+                            colors: [
+                              Colors.black.withOpacity(0.8),
+                              Colors.black.withOpacity(0.1),
+                            ])),
+                    child: const Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Text(
+                          "Best Design",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      )),
+    );
+  }
+
+  Widget promoCard(image) {
+    return AspectRatio(
+      aspectRatio: 2.62 / 3,
+      child: Container(
+        margin: const EdgeInsets.only(right: 15.0),
+        decoration: BoxDecoration(
+            color: Colors.orange,
+            borderRadius: BorderRadius.circular(20),
+            image:
+                DecorationImage(fit: BoxFit.cover, image: AssetImage(image))),
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient:
+                  LinearGradient(begin: Alignment.bottomRight, stops: const [
+                0.1,
+                0.9
+              ], colors: [
+                Colors.black.withOpacity(0.8),
+                Colors.black.withOpacity(0.1),
+              ])),
+        ),
+      ),
+    );
+  }
+}
